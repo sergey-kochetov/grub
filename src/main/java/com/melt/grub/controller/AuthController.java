fdb0d804632c2029c70deef9b57d7c2aa918dcef
sergey-kochetov <git.kochetov@gmail.com>
2019-02-18 19:59:07 +0300

    добавил reCapcha от Google v2

--- src/main/java/com/melt/grub/controller/AuthController.java
+++ src/main/java/com/melt/grub/controller/AuthController.java
@@ -1,20 +1,36 @@
 package com.melt.grub.controller;
 
+import com.melt.grub.model.ReCaptchaResponseDto;
 import com.melt.grub.model.RegisterDto;
+import com.melt.grub.service.ReCaptchaApiClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 public class AuthController {
+    @Autowired
+    private ReCaptchaApiClient reCaptchaApiClient;
 
     @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public void register(RegisterDto registerDto) {
         log.info("{}", registerDto);
+
+        ReCaptchaResponseDto reCaptchaResponse =
+                reCaptchaApiClient.verify(registerDto.getRecaptchaResponse());
+        log.info("{}", reCaptchaResponse);
+
+        if (!reCaptchaResponse.isSuccess()) {
+            throw new RuntimeException();
+        }
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity handleruntimeException(RuntimeException ex) {
+        return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
 }
